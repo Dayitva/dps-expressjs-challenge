bd737526ab6f776dddcb218e4811099570a3e2fc
Dayitva <dayitvagoel@gmail.com>
2024-07-23 12:22:30 +0700

    feat: Add database read queries for projects and reports
    This commit adds two new functions, `read_projects()` and `read_reports()`, to retrieve data from the database. These functions execute SQL queries to select all records from the `projects` and `reports` tables respectively. The retrieved data is logged to the console for debugging purposes.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import db from './services/db.service';
 
 dotenv.config();
 
@@ -15,3 +16,25 @@ app.listen(port, () => {
 app.get('/', function (req, res) {
 	res.send('API is working properly!');
 });
+
+app.get('/read_projects', function (req, res) {
+	read_projects();
+	res.send('API is working properly!');
+});
+
+app.get('/read_reports', function (req, res) {
+	read_reports();
+	res.send('API is working properly!');
+});
+
+async function read_projects() {
+	const sql = 'SELECT * FROM projects';
+	const projects = db.query(sql);
+	console.log(projects);
+}
+
+async function read_reports() {
+	const sql = 'SELECT * FROM reports';
+	const reports = db.query(sql);
+	console.log(reports);
+}
