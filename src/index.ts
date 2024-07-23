0a9023fd981880353b28c157c9399ffe8186dade
Dayitva <dayitvagoel@gmail.com>
2024-07-23 19:28:42 +0700

    feat: Add authentication middleware for API endpoints

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,23 @@ app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function authenticate(req: any, res: any, next: any) {
+	const token = req.headers['authorization'];
+
+	if (!token) {
+		return res.status(403).send('A token is required for authentication');
+	}
+
+	if (token !== 'Password123') {
+		return res.status(401).send('Invalid token');
+	}
+
+	next();
+}
+
+app.use(authenticate);
+
 app.get('/', function (req, res) {
 	res.send('API is working properly!');
 });
