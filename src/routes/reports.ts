f347a6a4dbfde2f5e38884202acf51ef175d9cbc
Dayitva <dayitvagoel@gmail.com>
2024-07-23 21:37:35 +0700

    feat: Add Special API endpoint and another one for fetching reports by project ID

--- src/routes/reports.ts
+++ src/routes/reports.ts
@@ -9,6 +9,13 @@ router.get('/', async function (req, res) {
 	res.json(reports);
 });
 
+router.get('/project/:projectid', async function (req, res) {
+	const projectid = req.params.projectid;
+	const reports = await read_reports_by_project(projectid);
+	console.log(reports);
+	res.json(reports);
+});
+
 router.post('/', async function (req, res) {
 	const id = req.body.id;
 	const text = req.body.text;
@@ -43,9 +50,61 @@ router.delete('/', async function (req, res) {
 	}
 });
 
+router.get('/special', async function (req, res) {
+	const reports = await fetch_special_reports();
+	console.log(reports);
+	res.json(reports);
+});
+
+async function fetch_special_reports() {
+	interface Report {
+		id: string;
+		text: string;
+		projectid: string;
+	}
+
+	const sql = 'SELECT * FROM reports';
+	const reports: Report[] = db.query(sql) as Report[];
+
+	const specialReports = reports.filter((report) => {
+		const wordCount: Record<string, number> = {};
+		const words = report.text.split(/[\s,.!?;:()]+/);
+		words.forEach((word) => {
+			const cleanWord = word.toLowerCase();
+			if (cleanWord) {
+				// Check if the word is not empty after trimming punctuation
+				if (wordCount[cleanWord]) {
+					wordCount[cleanWord]++;
+				} else {
+					wordCount[cleanWord] = 1;
+				}
+			}
+		});
+
+		console.log(wordCount);
+
+		for (let i = 0; i < words.length; i++) {
+			if (wordCount[words[i]] >= 3) {
+				return true;
+			}
+		}
+
+		return false;
+	});
+
+	return specialReports;
+}
+
 async function read_reports() {
 	const sql = 'SELECT * FROM reports';
 	const reports = db.query(sql);
+	return reports;
+}
+
+async function read_reports_by_project(projectid: string) {
+	const sql = 'SELECT * FROM reports WHERE projectid = @projectid';
+	const params = { projectid: projectid };
+	const reports = db.query(sql, params);
 	return reports;
 }
 
